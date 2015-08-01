2be73a87fbcae0c0213494285bd46fd6cd4aecaa
humanchimp <morphcham@gmail.com>
2015-07-31 20:21:36 -0700

    move getters to the prototype

---
+++ src/membrane.js
@@ -0,0 +1,47 @@
+const { keys, defineProperty } = Object;
+
+class Examplar {
+	constructor(target, dsl) {
+		this.target = target;
+		this.dsl = dsl;
+	}
+}
+
+export default function createMembraneFactory(calculatorFactory) {
+	let isInitialized = false;
+
+	class Membrane extends Examplar {
+		
+		constructor(target, dsl) {
+			super(target, dsl);
+			this.calculator = calculatorFactory(target, dsl);
+			this.initialize();
+		}
+
+		initialize() {
+			if (isInitialized) return;
+
+			keys(this.calculator).forEach(key => {
+				const value = this.calculator[key];
+				const isFunction = 'function' === typeof value;
+
+				defineProperty(Membrane.prototype, key,
+					isFunction ?
+					property({ get() { return value.call(this); }}) :
+					property({ value }));
+			});
+
+			isInitialized = true;
+		}
+
+		isInitialized() {
+			return isInitialized;
+		}
+	};
+	return (data, dsl) => new Membrane(data, dsl);
+}
+
+function property(descriptor) {
+	descriptor.enumerable = true;
+	return descriptor;
+}
--- src/node.js
+++ src/node.js
@@ -1,29 +1,9 @@
-const { keys, defineProperty } = Object;
+import createMembraneFactory from './membrane'
 
 export default class Node {
 	constructor(context, calculator) {
 		this.context = context;
 		this.calculator = calculator;
+		this.compute = createMembraneFactory(calculator);
 	}
-
-	compute(data, dsl) {
-		const calculator = this.calculator(data, dsl);
-		const membrane = keys(calculator).reduce((memo, key) => {
-			const value = calculator[key];
-			const isFunction = 'function' === typeof value;
-
-			defineProperty(memo, key, (
-				isFunction ?
-				property({ get() { return value.call(membrane) }}) :
-				property({ value })));
-
-			return memo;
-		}, {});
-		return membrane;
-	}
-}
-
-function property(descriptor) {
-	descriptor.enumerable = true;
-	return descriptor;
 }
